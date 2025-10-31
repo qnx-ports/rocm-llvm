8fa2d6cc26574cdc68330728fbb11aa8a6521c88
Felix Xing <fxing@qnx.com>
2025-10-31 09:26:08 -0400

    openmp: fix stdarg on arm

--- openmp/runtime/src/kmp_os.h
+++ openmp/runtime/src/kmp_os.h
@@ -220,7 +220,7 @@ typedef kmp_uint32 kmp_uint;
 // stdarg handling
 #if (KMP_ARCH_ARM || KMP_ARCH_X86_64 || KMP_ARCH_AARCH64 || KMP_ARCH_WASM) &&  \
     (KMP_OS_FREEBSD || KMP_OS_NETBSD || KMP_OS_OPENBSD || KMP_OS_DRAGONFLY ||  \
-     KMP_OS_LINUX || KMP_OS_WASI)
+     KMP_OS_LINUX || KMP_OS_WASI || KMP_OS_QNX)
 typedef va_list *kmp_va_list;
 #define kmp_va_deref(ap) (*(ap))
 #define kmp_va_addr_of(ap) (&(ap))
